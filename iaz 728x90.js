0e99d1a1eba45024eb0d3440448b158c698a0200
iazsibassam <iazsibassam@gmail.com>
2023-08-21 00:13:00 +0700

    Update iaz 728x90.js

--- iaz 728x90.js
+++ iaz 728x90.js
@@ -5,4 +5,4 @@
 		'width' : 728,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://hf5rbejvpwds.com/862d03e74e4b087ad3db003d36d42e4a/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://mosqueworking.com/862d03e74e4b087ad3db003d36d42e4a/invoke.js"></scr' + 'ipt>');
