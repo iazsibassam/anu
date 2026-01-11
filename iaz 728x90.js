5db5faaa8eca4a652e6d4f7b719513670119ad72
Mas Syapii <iazsibassam@gmail.com>
2026-01-12 00:05:18 +0700

    Update iaz 728x90.js

--- iaz 728x90.js
+++ iaz 728x90.js
@@ -1,8 +1,8 @@
 	atOptions = {
-		'key' : '862d03e74e4b087ad3db003d36d42e4a',
+		'key' : '943f580782d6524edf4418593144bbc1',
 		'format' : 'iframe',
 		'height' : 90,
 		'width' : 728,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://mosqueworking.com/862d03e74e4b087ad3db003d36d42e4a/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://programmesheadstoneconsult.com/943f580782d6524edf4418593144bbc1/invoke.js"></scr' + 'ipt>');
